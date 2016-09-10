18ea3375ecaa04dd4e87f1914f008a4802694bbd
kriescher1234 <klaus_home@gmx.de>
2016-09-10 12:41:42 +0200

    MissingFilesSearch extended
    MissingFilesSearch extended

--- MissingFilesSearch.exe.bat
+++ MissingFilesSearch.exe.bat
@@ -4,7 +4,8 @@ rem sample:
 rem MissingFilesSearch.exe /path:"E:\Working\Grim Dawn\mods\XADAIL-1\DAIL\database" /GrimDawnPath:"E:\Working\Grim Dawn\database"
 
 rem /path path to check
-rem /GrimDawnPath path to extracted GD extracted game files
+rem /GrimDawnPath path to extracted GD extracted game files (should not contain extracted DAIL stuff)
 rem /IgnorePattern ignore files that contain any of these patterns 
-MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL\database" /GrimDawnPath:"E:\Working\Grim Dawn\database" /IgnorePattern:"\sounds\;passive_totaldamageabsorption01;\loottables\\"
+rem /WhitePattern:"storm" filters files from /path parameter (if you are looking for a special folder for example)
+MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL\database" /GrimDawnPath:"E:\SteamLibrary\steamapps\common\Grim Dawn" /IgnorePattern:"\sounds\;petskill_abomination_scaling;Class29;passive_totaldamageabsorption01;perlevelx100;defense_undeadresists;resist_undead;melee_lifeleech_01;armor_passive;trap_resists;loottables;lootaffixes;m002_mutantarml01;m002_mutantarmr01" /WhitePattern:"skills"
 rem MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL - Survival\database" /GrimDawnPath:"E:\Working\Grim Dawn\database" /IgnorePattern:"\sounds\;\loottables\\"
--- Tools/MissingFilesSearch/MissingFilesSearch/Search/Searcher.cs
+++ Tools/MissingFilesSearch/MissingFilesSearch/Search/Searcher.cs
@@ -11,31 +11,51 @@ namespace MissingFilesSearch.Search
         private static int _count;
         private static int _percent;
         private static object _lock = new object();
+        private static string _pattern = string.Empty;        
 
-        public static List<Dto.FilesReference> GetFiles(Arguments.Args args, string pattern = "*")
+        public List<Dto.FilesReference> GetFiles(Arguments.Args args, string pattern = "*")
         {
             _counter = 0;
             _percent = 0;
+            _pattern = pattern;
+            var dbrFiles = GetDbrFiles(args);
             var files = Directory.GetFiles(args.Path, "*." + pattern, SearchOption.AllDirectories).ToList();
-            _count = files.Count;
+            List<string> whitePattern = GetWhitelistPattern(args);
+
             var grimFiles = Directory.GetFiles(args.GrimDawnPath, "*." + pattern, SearchOption.AllDirectories).ToList();
             grimFiles.AddRange(files);
-            return files.AsParallel()
-                 .Select(x => new Dto.FilesReference { Dbr = x, References = InitializeFilesReferences(x, pattern, grimFiles, args.IgnorePattern) })
+            if (whitePattern.Count > 0)
+            {
+                dbrFiles = dbrFiles.Where(x => whitePattern.Any(x.Contains)).ToList();
+            }
+            _count = dbrFiles.Count;
+            return dbrFiles.AsParallel()
+                 .Select(x => new Dto.FilesReference { Dbr = x, References = InitializeFilesReferences(x, pattern, grimFiles, args) })
                  .Where(x => x.References.Count > 0).ToList();
         }
 
-        private static List<string> InitializeFilesReferences(string file, string pattern, List<string> files, string ignorePattern)
+        protected virtual List<string> GetDbrFiles(Arguments.Args args)
+        {
+            return Directory.GetFiles(args.Path, "*.dbr", SearchOption.AllDirectories).ToList();
+        }
+
+        private static List<string> GetWhitelistPattern(Arguments.Args args)
+        {
+            return (string.IsNullOrWhiteSpace(args.WhitePattern) ? "" : args.WhitePattern)
+                            .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToLower()).ToList();
+        }
+
+        private List<string> InitializeFilesReferences(string file, string pattern, List<string> files, Arguments.Args args)
         {
             var references = new List<string>();
             var text = File.ReadAllLines(file).ToList();
-            var ignorePatterns = (string.IsNullOrWhiteSpace(ignorePattern) ? "!!!!" : ignorePattern)
-                .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var ignorePatterns = (string.IsNullOrWhiteSpace(args.IgnorePattern) ? "!!!!" : args.IgnorePattern)
+                .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToLower()).ToList();            
             references.AddRange(text.AsParallel().Where(x => x.EndsWith("." + pattern + ","))
                 .Where(x => !x.Split(new [] { ',' }, StringSplitOptions.RemoveEmptyEntries)[0].Equals("fileNameHistoryEntry", StringComparison.InvariantCultureIgnoreCase))
-                .Select(x => x.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)[1].Replace("/", "\\"))
+                .Select(x => x.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)[1].Replace("/", "\\"))                 
                 .SelectMany(x => x.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
-                .Where(x => !ignorePatterns.Any(y => x.Contains(y)))
+                .Where(x => !ignorePatterns.Any(y => x.Contains(y.ToLower())))
                 .Where(x => !files.Any(y => y.EndsWith(x, StringComparison.InvariantCultureIgnoreCase)))
                 .ToList());
             Increment();
@@ -52,7 +72,7 @@ namespace MissingFilesSearch.Search
                 if (update)
                 {
                     _percent = percent;                    
-                    Console.Write("\rValidating files: {0}/{1} ({2}%)", _counter, _count, percent);
+                    Console.Write("\rValidating ({3}) files: {0}/{1} ({2}%)", _counter, _count, percent, _pattern);
                 }
             }
         }
