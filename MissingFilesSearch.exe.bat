df86ad076d7cf96bd3ec4ff60e610e5378fc608f
kriescher1234 <klaus_home@gmx.de>
2016-09-11 12:38:16 +0200

    Added missing files
    Added missing files

--- MissingFilesSearch.exe.bat
+++ MissingFilesSearch.exe.bat
@@ -7,5 +7,5 @@ rem /path path to check
 rem /GrimDawnPath path to extracted GD extracted game files (should not contain extracted DAIL stuff)
 rem /IgnorePattern ignore files that contain any of these patterns 
 rem /WhitePattern:"storm" filters files from /path parameter (if you are looking for a special folder for example)
-MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL\database" /GrimDawnPath:"E:\SteamLibrary\steamapps\common\Grim Dawn" /IgnorePattern:"\sounds\;petskill_abomination_scaling;Class29;passive_totaldamageabsorption01;perlevelx100;defense_undeadresists;resist_undead;melee_lifeleech_01;armor_passive;trap_resists;loottables;lootaffixes;m002_mutantarml01;m002_mutantarmr01" /WhitePattern:"skills"
+MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL" /GrimDawnPath:"E:\SteamLibrary\steamapps\common\Grim Dawn" /IgnorePattern:"\sounds\;petskill_abomination_scaling;Class29;passive_totaldamageabsorption01;perlevelx100;defense_undeadresists;resist_undead;melee_lifeleech_01;armor_passive;trap_resists;loottables;old combat directory;lootaffixes;m002_mutantarml01;m002_mutantarmr01"
 rem MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL - Survival\database" /GrimDawnPath:"E:\Working\Grim Dawn\database" /IgnorePattern:"\sounds\;\loottables\\"
